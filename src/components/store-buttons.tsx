ab3e3aa15978cf891df6b4ac357668af3bace18f
Averton Silva Dias <verton3@gmail.com>
2025-12-27 20:35:38 +0000

    Tem como deixar o ícone da play Store colorido?  (_for element a_)

--- src/components/store-buttons.tsx
+++ src/components/store-buttons.tsx
@@ -2,12 +2,14 @@ import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/button';
 
 const PlayStoreIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg viewBox="0 0 32 32" {...props}>
-    <path d="M24,14.3,9.4,5.05a2,2,0,0,0-2,1.74V25.22a2,2,0,0,0,2,1.73L24,21.7a2,2,0,0,0,0-3.46l-6.3-3.63,6.3-3.64A2,2,0,0,0,24,14.3Z" />
-    <path d="M9.4,5.05,20.89,12,9.4,18.89Z" />
-    <path d="m9.4,26.95,11.5-6.6-11.5-6.88Z" />
+  <svg viewBox="0 0 32 32" width="24" height="24" {...props}>
+    <path fill="#00d8ff" d="M28.8,14.53,5.4,0.27A2.3,2.3,0,0,0,2,2.25V29.75a2.3,2.3,0,0,0,3.4,2L28.8,17.47a2.31,2.31,0,0,0,0-2.94Z" />
+    <path fill="#00a0ff" d="M2,29.75,17.6,16,2,2.25Z" />
+    <path fill="#ffc928" d="m22.8,18.52-5.2-5.22-9-8.75L2,2.25V29.75l6.58-6.38Z" />
+    <path fill="#f25522" d="M22.8,13.3,17.6,16l-5.2,5.22L10.9,19.7,28.8,14.53Z" />
   </svg>
 );
+
 
 const AppleIcon = (props: React.SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
@@ -58,7 +60,7 @@ export function PlayStoreButton({ href, className }: { href: string, className?:
   return (
     <Button variant="outline" asChild className={cn("bg-black text-white hover:bg-black/80 hover:text-white", className)}>
       <a href={href} target="_blank" rel="noopener noreferrer">
-        <PlayStoreIcon className="h-5 w-5 mr-2 fill-current" />
+        <PlayStoreIcon className="h-5 w-5 mr-2" />
         <div>
           <p className="text-xs">Disponível no</p>
           <p className="text-sm font-semibold">Google Play</p>
